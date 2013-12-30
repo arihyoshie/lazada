4379c91225ef7a4b95969afea9f94898b172ab41
Timothy Washington <twashing@gmail.com>
2013-12-30 11:06:15 -0800

    -- ignoreing 'changes' scratch file -- adding TODO -- adding enlive lib -- just getting a link nested structures

--- src/lazada/core.clj
+++ src/lazada/core.clj
@@ -1,6 +1,24 @@
-(ns lazada.core)
+(ns lazada.core
+  (:import java.net.URL)
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;; index page
+(def lindex
+  (-> "http://lazada.com.ph" URL. html/html-resource))
+
+
+;; get top categories
+(def nodes (html/select lindex [:a.catArrow]))
+
+
+;; get sibling .navLayer, for each top category
+(def ff (first nodes))
+(def nl (html/select lindex [(html/left :a.catArrow)]))
+
+
+;; .navLayer > .navLayerSub > .sbnc* > .bsnclco > a.bsncLink
+(def blinks
+  (html/select
+   nl
+   [:.navLayer :> :.navLayerSub :> [[:div (html/attr-starts :class "sbnc")]] :> :.bsnclco :> :a.bsncLink]))
