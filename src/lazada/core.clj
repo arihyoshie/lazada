488f914d0cc725c39e53882ab517f4254dbbafcc
Timothy Washington <twashing@gmail.com>
2013-12-30 12:29:47 -0800

    -- finding sublinks and titles for each

--- src/lazada/core.clj
+++ src/lazada/core.clj
@@ -22,3 +22,32 @@
   (html/select
    nl
    [:.navLayer :> :.navLayerSub :> [[:div (html/attr-starts :class "sbnc")]] :> :.bsnclco :> :a.bsncLink]))
+
+
+;; crawl lazada.com.ph. recursively pull in all pages, record
+;;   i. top category (.catArrow)
+;;   ii. sub-category (.bsnch, .bsnclco)
+;;   iii. target category (.bsncLink)
+
+;; export to edn, json, csv, html
+
+
+
+;; find all products in each category
+(def subindex
+  (-> "http://www.lazada.com.ph/shop-dslr-slr" URL. html/html-resource))
+
+(def sublinks
+  (html/select subindex [:#productsCatalog :li :ul.subGroupProducts :li.unit :.itm :a.itm-link]))
+
+(def sublink-names (map #(-> % :attrs :title)
+                        core/sublinks))
+
+;; record first 5 in each category
+(def sublink-first-five (take 5 sublink-names))
+
+
+;; A) Get index page
+;; B) Build category tree
+;; C) Crawl sub-pages
+;; D) ...
