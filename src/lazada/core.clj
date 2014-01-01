52de64243a065cd747fbe5499d6a8a6c15f78dcc
Timothy Washington <twashing@gmail.com>
2013-12-31 16:38:24 -0800

    -- first success with report.txt

--- src/lazada/core.clj
+++ src/lazada/core.clj
@@ -7,9 +7,3 @@
 (defn get-page [purl]
   (println "Getting page: " purl)
   (-> purl URL. html/html-resource))
-
-
-;; export to edn, json, html
-#_(require '[clojure.pprint :refer :all])
-#_(spit "xx.edn" (with-out-str (pprint xx)))
-#_(spit "xx.json" (with-out-str (pprint (json/write-str {:a 1 :b 2}))))
